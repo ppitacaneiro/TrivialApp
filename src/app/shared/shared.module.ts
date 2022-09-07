08baec514bbd53610b67a6532648f21a42f7973b
Pablo <pablo.pita@tokiota.com>
2022-09-07 10:19:31 +0200

    created shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { HeaderComponent } from './components/header/header.component';
+import { TextHeaderComponent } from './components/text-header/text-header.component';
+import { GenericButtonComponent } from './components/generic-button/generic-button.component';
+import { InputTextComponent } from './components/input-text/input-text.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    InputTextComponent,
+    GenericButtonComponent,
+    TextHeaderComponent,
+    HeaderComponent,
+  ],
+  imports: [FormsModule, CommonModule, IonicModule, ReactiveFormsModule],
+  exports: [
+    InputTextComponent,
+    GenericButtonComponent,
+    TextHeaderComponent,
+    HeaderComponent,
+  ],
+})
+export class SharedModule {}
