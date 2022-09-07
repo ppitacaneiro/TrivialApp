553a278c2cc581edc47273bd45758dee22b8c59e
Pablo <pablo.pita@tokiota.com>
2022-09-07 10:18:55 +0200

    player form created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { MessageService } from './../services/message.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  start() {
+    if (this.loginForm.invalid) {
+      this.messageService.presentToast('Form is invalid');
+      return;
+    }
+
+    // TODO : send player data to NET CORE API
+
+    this.router.navigate(['game-options']);
+  }
 }
