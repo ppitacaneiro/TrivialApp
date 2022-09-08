6163872c4d149d90dfa58a565084a008b76d2b7d
Pablo <pablo.pita@tokiota.com>
2022-09-08 16:55:54 +0200

    path results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'questions',
     loadChildren: () => import('./questions/questions.module').then( m => m.QuestionsPageModule)
+  },  {
+    path: 'results',
+    loadChildren: () => import('./results/results.module').then( m => m.ResultsPageModule)
   },
+
 ];
 
 @NgModule({
