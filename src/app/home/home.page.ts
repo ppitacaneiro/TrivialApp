335cb4e2f574a43602ee407ab0bf90ecb624df9b
Pablo <pablo.pita@tokiota.com>
2022-09-21 13:18:12 +0200

    saving player info

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,6 @@
+import { PlayerService } from './../services/player.service';
+import { Player } from './../interfaces/player';
+import { TriviaApiService } from './../services/trivia-api.service';
 import { MessageService } from './../services/message.service';
 import { Component, OnInit } from '@angular/core';
 import {
@@ -19,7 +22,9 @@ export class HomePage implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private messageService: MessageService,
-    private router: Router
+    private router: Router,
+    private triviaApiService: TriviaApiService,
+    private playerService: PlayerService
   ) {}
 
   ngOnInit(): void {
@@ -37,7 +42,25 @@ export class HomePage implements OnInit {
       return;
     }
 
-    // TODO : send player data to NET CORE API
+    const player: Player = {
+      firstName: this.loginForm.controls.firstName.value,
+      lastName: this.loginForm.controls.lastName.value,
+      age: +this.loginForm.controls.age.value,
+      email: this.loginForm.controls.email.value,
+    };
+
+    // TODO : Unsuscribe observable
+    // TODO : send player data to .NET CORE API
+    // Handle HTTP errors
+    this.triviaApiService.savePlayerData(player).subscribe(
+      (response) => {
+        player.id = response.id;
+        this.playerService.setPlayer(player);
+      },
+      (error) => {
+        this.messageService.presentToast(error.message);
+      }
+    );
 
     this.router.navigate(['game-options']);
   }
